6ab86d905629d9dd950cddd4b1bb9a5fbc24ab6a
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2019-10-04 09:23:52 +0900

    Add files via upload

---
+++ camera1TEST.py
@@ -0,0 +1,10 @@
+import cv2
+url = 'rtsp://admin:88888888@172.24.1.63:10554/tcp/av0_0'
+#192.168.0.4 172.24.1.63
+cap = cv2.VideoCapture(url)
+while True:
+    ret, image = cap.read()
+    cv2.imshow('stream', image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
